7f5d56b22a0aeafaa736ba2d6e98004fb47b2849
DeveloperKubilay <kubiss52@yahoo.com>
2025-06-08 18:13:31 +0300

    chore: initialize package.json for image-to-string project with dependencies and metadata

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# image-to-string
-Best auto image to text for windows
+# image-to-string ⚠️💀
 
-Installation:`npm init -y && npm i puppeteer fs child_process`
+> This module is **DISABLED** now! Don't waste your time tryna get it working! ❌
+
+Best auto image to text for windows that *used to be* working but now it's straight up broken. 📸➡️📝
+
+Installation (if you still wanna try for some reason): 
+```bash
+npm init -y && npm i puppeteer fs child_process
+```
+
+**Source:** Based on original code that no longer functional with recent updates [1]
+
+[1] Google image search API changes have affected this tool's functionality.
--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 var exec = require('child_process').exec
 fs = require('fs')
 puppeteer = require('puppeteer')
-setTimeout = require('timers/promises').setTimeout
+const sleep = require('timers/promises').setTimeout
 async function start() {
-    console.log('Developed by kubi')
-    try { await fs.unlinkSync("./image.png") } catch { }
-    await exec(`Add-Type -AssemblyName System.Windows.Forms;[System.Drawing.Bitmap][System.Windows.Forms.Clipboard]::GetDataObject().getimage().Save('${__dirname + "\\image.png"}' , [System.Drawing.Imaging.ImageFormat]::Png)`, { 'shell': 'powershell.exe' })
-    await setTimeout(2000)
-    try { await fs.readFileSync("./image.png"); console.log("Please wait") } catch { console.log("Not found text"); process.exit(1); }
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://images.google.com",{waitUntil: 'load',timeout: 0});
-    await page.waitForSelector('div > div > div > div:nth-child(10)');
-    await page.click("div > div > div > div:nth-child(10)")
-    await page.waitForSelector('c-wiz > div > div > div > div > div > div > span'); 
-    const [fileChooser] = await Promise.all([
-        page.waitForFileChooser(),
-        page.evaluate(() => document.querySelector("c-wiz > div > div > div > div > div > div > span").click())
-      ]);
-    await fileChooser.accept([__dirname+"\\image.png"]);
-    await setTimeout(7000);
-    await page.evaluate(() => document.querySelector("#ucj-4 > span").click())
-    await setTimeout(3000);
-    await page.evaluate(() => document.querySelector("c-wiz > div > div > div > div > div > div > div > div > div > div > button").click())
-    await setTimeout(5000);
-    var text = await page.evaluate(()=>
+  console.log('Developed by kubi')
+  try { await fs.unlinkSync("./image.png") } catch { }
+  await exec(`Add-Type -AssemblyName System.Windows.Forms;[System.Drawing.Bitmap][System.Windows.Forms.Clipboard]::GetDataObject().getimage().Save('${__dirname + "\\image.png"}' , [System.Drawing.Imaging.ImageFormat]::Png)`, { 'shell': 'powershell.exe' })
+  await sleep(2000)
+  
+  try { await fs.readFileSync("./image.png"); console.log("Please wait") } catch { console.log("Not found text"); process.exit(1); }
+  const browser = await puppeteer.launch({headless:false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process'] });
+  const page = await browser.newPage();
+  await page.goto("https://images.google.com", { waitUntil: 'load', timeout: 0 });
+  await page.waitForSelector('div > div > div > div:nth-child(10)');
+  await page.click("div > div > div > div:nth-child(10)")
+  await page.waitForSelector('c-wiz > div > div > div > div > div > div > span');
+  const [fileChooser] = await Promise.all([
+    page.waitForFileChooser(),
+    page.evaluate(() => document.querySelector("c-wiz > div > div > div > div > div > div > span").click())
+  ]);
+  await fileChooser.accept([__dirname + "\\image.png"]);
+  await sleep(7000);
+  await page.evaluate(() => document.querySelector("#ucj-4 > span").click())
+  await sleep(3000);
+  await page.evaluate(() => document.querySelector("c-wiz > div > div > div > div > div > div > div > div > div > div > button").click())
+  await sleep(5000);
+  var text = await page.evaluate(() =>
     (document.querySelector("c-wiz > div > div > div > c-wiz > div > div > div > div > span > div > div:nth-child(2)").innerHTML)
-    .replace('"', '').replace('"', '').replaceAll("&gt;",">").replaceAll("&lt;","<").replaceAll("&amp;","&"))
-   await fs.writeFileSync("text.txt", text);
-   exec("start text.txt")
-   await setTimeout(2000);
-   process.exit(0);
+      .replace('"', '').replace('"', '').replaceAll("&gt;", ">").replaceAll("&lt;", "<").replaceAll("&amp;", "&"))
+  await fs.writeFileSync("text.txt", text);
+  exec("start text.txt")
+  await sleep(2000);
+  process.exit(0);
 } start()
